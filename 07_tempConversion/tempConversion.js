5c2eba17a45285604fbe1a38ec7531c2901f484f
Shem Jaoko <jaokoshem@gmail.com>
2024-03-15 09:38:18 +0300

    Completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let cel = 0
+  cel = (far - 32) * (5/9)
+  let result = Math.round(cel * 10) / 10
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let far = 0
+  far = (cel * 9/5) + 32
+  let result = Math.round(far * 10) / 10
+  return result
 };
 
 // Do not edit below this line
