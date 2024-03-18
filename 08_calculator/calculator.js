5a6c8ad7f58545f1699b2f419d9fdaa031ee5dad
Shem Jaoko <jaokoshem@gmail.com>
2024-03-18 22:06:19 +0300

    Finished calculator problem

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let result = 0
+  for(let i = 0; i < a.length; i++) {
+    result += a[i]
+  }
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let result = 1
+  for(i=0; i<a.length; i++) {
+    result *= a[i]
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let result = Math.pow(a,b)
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let result = 1
+	for(i=a; i>=1; i--) {
+      result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
